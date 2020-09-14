c7561f63e23be0b9fe868004eb95fc89cdc02b36
anime-memes <zombiefaraday@gmail.com>
2020-09-14 10:20:06 +0300

    Use async stream instead of simple tasks

--- apps/repo_list_fetcher/lib/repo_list_fetcher/fetchers/github.ex
+++ apps/repo_list_fetcher/lib/repo_list_fetcher/fetchers/github.ex
@@ -5,6 +5,7 @@ defmodule RepoListFetcher.Fetchers.Github do
   Fetching data from Github API performs using Tasks
   """
   @behaviour RepoListFetcher.Fetchers.Fetcher
+  @timeout 60_000
 
   alias RepoListFetcher.Fetchers.Github.{AdditionalInfo, Readme}
 
@@ -14,22 +15,25 @@ defmodule RepoListFetcher.Fetchers.Github do
   """
   def get_actual_list do
     Readme.parse()
-    |> Enum.map(&get_additional_info/1)
-    |> Task.yield_many(60_000)
-    |> Enum.map(fn {task, res} -> res || Task.shutdown(task, :brutal_kill) end)
+    |> get_additional_info()
     |> Enum.map(fn
       {:ok, result} -> result
       _ -> []
     end)
   end
 
-  # Creates task that is not linked to caller and instead is supervised by Task.Supervisor
-  defp get_additional_info(item) do
-    Task.Supervisor.async_nolink(
+  # Creates tasks stream
+  # No tasks linked to caller, so one fail won't crash the system/
+  # Instead tasks are supervised by Task.Supervisor
+  defp get_additional_info(items) do
+    Task.Supervisor.async_stream_nolink(
       RepoListFetcher.TaskSupervisor,
+      items,
       AdditionalInfo,
       :request,
-      [item],
+      [],
+      max_concurrency: 50,
+      timeout: @timeout,
       shutdown: :brutal_kill
     )
   end
